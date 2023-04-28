8df395bd32d7ab9ad6b91a67c56a026e5f478909
Moslem Hadi <moslem.hady@gmail.com>
2023-04-28 13:05:10 +0330

    project added

---
+++ src/Presentation/Ships.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ships.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
