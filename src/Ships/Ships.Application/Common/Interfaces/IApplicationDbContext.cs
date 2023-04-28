8c8e3f6df2b0958cd34c5e8af937076e2d73d1bf
Moslem Hadi <moslem.hady@gmail.com>
2023-04-28 16:47:30 +0330

    added dbContext and mapping

---
+++ src/Ships/Ships.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Ships.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ships.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<Ship> Ships { get; }
+
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
